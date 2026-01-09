bf7ee53db0d706e7719ae94e707812068250e377
ashifsekh <aashifsekhh@gmail.com>
2026-01-09 19:14:39 +0530

    Implement Chainlink VRF integration in Raffle contract

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -24,6 +24,8 @@
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+import {VRFCoordinatorV2Interface} from "chainlink/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "chainlink/src/v0.8/vrf/VRFConsumerBaseV2.sol";
 
 /**
  * @title A Simple Raffle Contract
@@ -32,7 +34,7 @@ pragma solidity ^0.8.19;
  * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2{
 
     // Errors
     error Raffle__NotEnoughEthSent();
@@ -42,13 +44,17 @@ contract Raffle {
     uint256 private immutable i_interval;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
+    // Chainlink VRF related variables
+    address immutable i_vrfCoordinator;
 
 
     event EnteredRaffle(address indexed player);
 
-    constructor (uint256 entranceFee, uint256 interval) {
+    constructor (uint256 entranceFee, uint256 interval, address vrfCoordinator) {
         i_entranceFee = entranceFee;
         i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = vrfCoordinator;
     }
 
     function enterRaffle() external payable {
@@ -67,6 +73,14 @@ contract Raffle {
         // 3.Automatically called 
         // check to see if enough time has passed
         if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+
+        uint256 requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+    );
 
     }
 
